eb722865d5a21d01b5b052279c472190e6662c5b
Athulx7 <athulkrishnapr2004@gmail.com>
2025-09-28 20:36:52 +0530

    chat section

--- src/containers/AIAsistance/MainAiAssistancePage.jsx
+++ src/containers/AIAsistance/MainAiAssistancePage.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import ChatSection from '../../Components/aiAsistance/ChatSection'
 
 function MainAiAssistancePage() {
     return (
         <div className='bg-primary dark:bg-gray-800 dark:text-white pb-2'>
             <div className='pt-10 px-8'>
                 <div className='text-3xl font-bold'>
-                    AI Assistant
+                    Agrisense AI
                 </div>
                 <div className='py-3 text-gray-700 dark:text-gray-300'>Get smart farming advice and personalized recommendations</div>
             </div>
+            <ChatSection />
         </div>
     )
 }
